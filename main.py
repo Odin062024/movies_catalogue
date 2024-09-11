e705e70e965e8f7b4e9f8f1b4ac8ff245c4ca635
Wojciech Mazur <odinkodilla@int.pl>
2024-09-11 18:46:45 +0200

    Add backdrop_path

--- main.py
+++ main.py
@@ -3,17 +3,25 @@ import tmdb_client
 
 app = Flask(__name__)
 
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
 @app.route('/')
 def homepage():
     # Pobieramy 8 filmów
     movies = tmdb_client.get_movies(how_many=8)
     return render_template("homepage.html", movies=movies)
 
-@app.context_processor
-def utility_processor():
-    def tmdb_image_url(path, size="w342"):
-        return tmdb_client.get_poster_url(path, size)
-    return {"tmdb_image_url": tmdb_image_url}
+
+@app.route('/movie/<int:movie_id>')
+def movie_details(movie_id):
+    # Pobieramy szczegóły filmu i obsadę
+    movie = tmdb_client.get_movie_details(movie_id)
+    cast = tmdb_client.get_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=movie, cast=cast)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,12 +1,14 @@
 import requests
 
+api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDJiMDQxNTg3NzYxMjIxNTE0MWU3MDlhMWIzMTFiZiIsIm5iZiI6MTcyNTk5MjQ2MS40NzM3ODksInN1YiI6IjY2ZTA0OGZmNzE4OGM5OWZiOTI4ZTNmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mAe2nxB0avLMS9MEys-iPJgo74lQBVF27GL7uwYzhCQ"
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDJiMDQxNTg3NzYxMjIxNTE0MWU3MDlhMWIzMTFiZiIsIm5iZiI6MTcyNTk5MjQ2MS40NzM3ODksInN1YiI6IjY2ZTA0OGZmNzE4OGM5OWZiOTI4ZTNmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mAe2nxB0avLMS9MEys-iPJgo74lQBVF27GL7uwYzhCQ"
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
     response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
     return response.json()
 
 def get_poster_url(poster_api_path, size="w342"):
@@ -16,3 +18,22 @@ def get_poster_url(poster_api_path, size="w342"):
 def get_movies(how_many):
     data = get_popular_movies()
     return data["results"][:how_many]
+
+def get_movie_details(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()["cast"]
+
